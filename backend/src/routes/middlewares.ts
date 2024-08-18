96ffb517025b7a49b2870049b6bf14f1fd378821
krishna <krishh16@yahoo.com>
2024-08-18 10:04:28 +0000

    added env variables in the file

--- backend/src/routes/middlewares.ts
+++ backend/src/routes/middlewares.ts
@@ -1,17 +1,18 @@
 import { NextFunction, Response, Request } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken"
 
-const JSON_SECRET = "Some secret key"
 export function authMiddleWare(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.header("Authorization") ?? "";
 
     try {
-        const decoded = jwt.verify(authHeader, JSON_SECRET) as JwtPayload;
+        // @ts-ignore
+        const decoded = jwt.verify(authHeader, process.env.JWT_SECRET) as JwtPayload;
         if (decoded.userId) {
             //@ts-ignore
             req.userId = decoded.userId;
             return next();
         } else {
+           
             res.status(403).json({
                 msg: "You are not logged in"
             })
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken"
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { authMiddleWare } from "./middlewares";
+
 import { PrismaClient } from "@prisma/client";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 const route = Router();
+//@ts-ignore
+const s3Client = new S3Client({
+    credentials: {
+        accessKeyId: process.env.AMAZON_ACCESS_KEY,
+        secretAccessKey: process.env.AMAZON_SECRET_KEY
+    },
+    region: "eu-north-1"
+});
+
 
 const prisma = new PrismaClient()
 route.post('/v1/signin', async (req, res) => {
@@ -13,26 +26,53 @@ route.post('/v1/signin', async (req, res) => {
 
     if (existingUser) {
         const token = jwt.sign({
-            address: walletAddress
-        }, "Some secret key")
-        res.json({token})
+            userId: existingUser.id
+            //@ts-ignore
+        }, process.env.JWT_SECRET)
+
+        res.json({
+            token
+        })
+
     } else {
-        await prisma.user.create({
+        const someUser = await prisma.user.create({
             data: {
                 address: walletAddress
             }
         })
         const token = jwt.sign({
-            address: walletAddress
+            userId: someUser.id
         }, "Some secret key")
 
-        res.json({token})
-    }
+        res.json({
+            token
+        })
+        }
 
 })
 
-route.get("/getPresignedUrl", (req, res) => {
-    
+route.get("/v1/getPresignedUrl", authMiddleWare, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+
+    const {url, fields} = await createPresignedPost(s3Client, {
+        Bucket: "decentralized-feverrr",
+        Key: `images/${userId}/${Math.random()}/image.jpg`,
+        Conditions: [
+            ["content-length-range", 0, 5 * 1024 * 1024] // 5 mb max
+        ],
+        Fields: {
+            "Content-Type": 'image/jpg'
+        },
+        Expires: 3600
+    })
+
+
+    console.log(url, fields)
+    res.json({
+        preSignedUrl: url,
+        fields   
+    })
 })
 
 export default route;
